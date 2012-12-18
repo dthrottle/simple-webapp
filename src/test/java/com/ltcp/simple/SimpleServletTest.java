91eff5216395afa3e20e2dc9abb5253b872c7f26
Ted Donald <ted.donald@gmail.com>
2012-12-18 15:47:00 -0500

    Added Test Class

---
+++ src/test/java/com/ltcp/simple/SimpleServletTest.java
@@ -0,0 +1,14 @@
+package com.ltcp.simple;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleServletTest {
+
+	@Test
+	public void testDoGetHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+}
