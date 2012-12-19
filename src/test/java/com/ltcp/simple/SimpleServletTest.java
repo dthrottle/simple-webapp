2cb3413bd4ea1dcabb8b4775596be0a44ca97e44
tdonald <tdonald@tcpartners.com>
2012-12-19 15:13:18 -0500

    Added jetty ServletTester and updated SimpleServletTest.

--- src/test/java/com/ltcp/simple/SimpleServletTest.java
+++ src/test/java/com/ltcp/simple/SimpleServletTest.java
@@ -3,12 +3,27 @@ package com.ltcp.simple;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mortbay.jetty.testing.*;
 
 public class SimpleServletTest {
 
 	@Test
-	public void testDoGetHttpServletRequestHttpServletResponse() {
-		fail("Not yet implemented");
+	public void testDoGetHttpServletRequestHttpServletResponse() 
+			throws Exception {
+		ServletTester tester = new ServletTester();
+		tester.addServlet(SimpleServlet.class, "/simple");
+		tester.start();
+		HttpTester request = new HttpTester();
+		request.setMethod("GET");
+		request.setHeader("Host", "tester");
+		request.setURI("/simple");
+		request.setVersion("HTTP/1.0");
+		HttpTester response = new HttpTester();
+		response.parse(tester.getResponses(request.generate()));
+		assertEquals(200,response.getStatus());
+		String content = response.getContent();
+		System.out.println(content);
+		assertEquals("SimpleServlet Hello World!",content.trim());
 	}
 
 }
